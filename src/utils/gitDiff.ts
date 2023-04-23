15604cbb009b4715be9e5960cc7d83e93751cf45
d556f8 <l.juhyeonni@gmail.com>
2023-04-23 21:45:27 +0900

    feat: Add function for retrieving git diff of a specified file path

---
+++ src/utils/gitDiff.ts
@@ -0,0 +1,27 @@
+import { exec } from 'child_process';
+
+function gitDiff(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(`git diff ${filePath}`, (err, stdout) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+async function getDiff(filePath: string) {
+  const diff = await gitDiff(filePath);
+
+  if (!diff) {
+    throw new Error(
+      'there is no diffrences\nwhen you add new file, you should generate commit message manually',
+    );
+  }
+
+  return diff;
+}
+
+export { getDiff };
