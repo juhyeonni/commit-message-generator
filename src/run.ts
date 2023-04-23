88c0d7ad4ece571bd50593aba4f267f66bfd970e
d556f8 <l.juhyeonni@gmail.com>
2023-04-23 21:42:20 +0900

    feat(run): add support for generating commit messages based on git diff

--- src/run.ts
+++ src/run.ts
@@ -1,9 +1,17 @@
 import CommitGenerator from './commit-generator';
 import { CommitRule, Configuration } from './interfaces';
+import { AdditionalOptions } from './interfaces/AdditionalOptions.interface';
 import { getCommitRuleJson } from './utils/commitRule';
 import { createOpenAIApi } from './utils/openAIApi';
+import { getDiff } from './utils/gitDiff';
 
-async function run(reqMsg: string, apiKey: string): Promise<void> {
+async function run(
+  reqMsg: string,
+  apiKey: string,
+  addtionalOptions: AdditionalOptions,
+): Promise<void> {
+  const { diffPath } = addtionalOptions;
+
   const rule: CommitRule = getCommitRuleJson();
   const openai = createOpenAIApi(apiKey);
 
@@ -11,8 +19,24 @@ async function run(reqMsg: string, apiKey: string): Promise<void> {
     rule,
     openai,
   };
-
   const commitGenerator = new CommitGenerator(configuration);
+
+  if (diffPath) return runWithDiff(commitGenerator, diffPath);
+  if (reqMsg) return runCommon(commitGenerator, reqMsg);
+}
+
+async function runCommon(commitGenerator: CommitGenerator, reqMsg: string) {
+  const msg = await commitGenerator.genCommitMsg(reqMsg);
+
+  console.log(msg);
+}
+
+async function runWithDiff(commitGenerator: CommitGenerator, diffPath: string) {
+  const diff = await getDiff(diffPath);
+  const reqMsg =
+    "Below is the result of a git diff. Check out the diffrences and figure out what's changed." +
+    diff;
+
   const msg = await commitGenerator.genCommitMsg(reqMsg);
 
   console.log(msg);
